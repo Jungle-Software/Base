afcad70a4251467e326102ee7f64da7de0b8c7c2
KELLOGGS522 <laguimauve522@gmail.com>
2023-04-30 14:21:30 -0400

    Fixed issue with env variables

--- config/firebase.ts
+++ config/firebase.ts
@@ -8,17 +8,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 const firebaseConfig = {
-    apiKey: [process.env.NEXTJS_FIREBASE_API_KEY],
-    authDomain: process.env.NEXTJS_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXTJS_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXTJS_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXTJS_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXTJS_FIREBASE_APP_ID,
-    measurementId: process.env.NEXTJS_FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-export const auth = getAuth();
+export const auth = getAuth(app);
